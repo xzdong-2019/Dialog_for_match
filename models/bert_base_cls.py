833c55c9d64d73ee2204172e676cdd736dd74f88
xzdong <1336259552@qq.com>
2020-06-16 19:37:19 +0800

    为每个word的表示使用gru，最终表示仍然使用gru,并且gru的初始化采用随机初始化的结果

--- models/bert_base_cls.py
+++ models/bert_base_cls.py
@@ -21,8 +21,10 @@ class BERTbase(nn.Module):
     if self.hparams.do_eot and self.hparams.model_type == "bert_base_ft":
       self._bert_model.resize_token_embeddings(self._bert_model.config.vocab_size + 1) # [EOT]
     self.response_span = self.hparams.max_dialog_len//10
-    self.gru = nn.GRU(self.hparams.bert_hidden_dim, self.hparams.bert_hidden_dim)
-    self.softmax = nn.Softmax(dim=1)
+    self.gru_utt = nn.GRU(self.hparams.bert_hidden_dim, self.hparams.bert_hidden_dim)
+    self.gru_res = nn.GRU(self.hparams.bert_hidden_dim, self.hparams.bert_hidden_dim)
+    self.gru_all = nn.GRU(self.hparams.bert_hidden_dim, self.hparams.bert_hidden_dim)
+    #self.softmax = nn.Softmax(dim=1)
 
     self._classification = nn.Sequential(
       nn.Dropout(p=1 - self.hparams.dropout_keep_prob),
@@ -35,8 +37,27 @@ class BERTbase(nn.Module):
       token_type_ids=batch["segment_ids"],
       attention_mask=batch["attention_mask"]
     )
-    uttrance_logits = torch.stack([bert_outputs[:,i*self.response_span,:] for i in range(10)], 1) # bs, bert_output_size
-    respones_logits = bert_outputs[:, self.hparams.max_dialog_len,:]
+    #import pdb
+    #pdb.set_trace()
+    uttrance_logits = torch.stack([bert_outputs[:,i*self.response_span:(i+1)*self.response_span,:] for i in range(10)], 1) # bs, bert_output_size
+    respones_logits = bert_outputs[:, self.hparams.max_dialog_len:,:]
+    
+    
+    uttrance_logits = uttrance_logits.view(-1,self.response_span,self.hparams.bert_hidden_dim)
+    uttrance_logits = uttrance_logits.permute(1,0,2)
+    _, uttrance_logits =  self.gru_utt(uttrance_logits)
+    uttrance_logits = uttrance_logits.view(-1, 10, self.hparams.bert_hidden_dim)
+    
+    respones_logits = respones_logits.permute(1,0,2)
+    _,respones_logits = self.gru_res(respones_logits)
+    respones_logits = respones_logits.permute(1,0,2)
+    #respones_logits = respones_logits.view(-1,1,self.hparams.bert_hidden_dim)
+    
+    utt_res_logits = torch.cat((uttrance_logits,respones_logits),1)
+    utt_res_logits = utt_res_logits.permute(1,0,2)
+    _, cls_logits = self.gru_all(utt_res_logits)
+    
+    cls_logits = cls_logits.view(-1, self.hparams.bert_hidden_dim)
     
     # response
     #response = respones_logits.view(-1,self.hparams.bert_hidden_dim,1) # version 2.0 b*d*1
@@ -48,11 +69,11 @@ class BERTbase(nn.Module):
     
     #cls_logits = torch.cat((uttrance_logits,respones_logits),1)
     
-    uttrance_logits = uttrance_logits.permute(1, 0, 2)
+    #uttrance_logits = uttrance_logits.permute(1, 0, 2)
    
-    respones_logits = respones_logits.view(1, -1, self.hparams.bert_hidden_dim).contiguous()
-    _, cls_logits = self.gru(uttrance_logits, respones_logits)
-    cls_logits = cls_logits.view(-1, self.hparams.bert_hidden_dim)
+    #respones_logits = respones_logits.view(1, -1, self.hparams.bert_hidden_dim).contiguous()
+    #_, cls_logits = self.gru(uttrance_logits, respones_logits)
+    #cls_logits = cls_logits.view(-1, self.hparams.bert_hidden_dim)
     logits = self._classification(cls_logits) # bs, 1
     logits = logits.squeeze(-1)
 
